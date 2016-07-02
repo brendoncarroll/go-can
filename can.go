38fee0e556349deedf6553e22aa7829772d11e35
Brendon Carroll <brendonwcarroll@gmail.com>
2016-07-02 09:43:18 -0400

    Use net package to lookup interface index

--- can.go
+++ can.go
@@ -2,53 +2,42 @@
 
 package can
 
-// #include <sys/ioctl.h>
-// #include <net/if.h>
-// #include <sys/types.h> 
-// #include <sys/socket.h> 
-// #include <sys/ioctl.h> 
-// #include <net/if.h>
+// #include <sys/socket.h>
 //
-// #include <linux/can.h> 
-// #include <linux/can/raw.h> 
-// #include <string.h>
-// #include <stdlib.h>
-// 
-// int setupCAN(int fd, char *name) {
-//   struct ifreq ifr; 
-//   strcpy(ifr.ifr_name, name);
-//   ioctl (fd,  SIOCGIFINDEX, &ifr);
-//   
+// #include <linux/can.h>
+// #include <linux/can/raw.h>
+//
+// int setupCAN(int fd, int ifindex) {
 //   struct sockaddr_can addr;
 //   addr.can_family = AF_CAN;
-//   addr.can_ifindex = ifr.ifr_ifindex;
+//   addr.can_ifindex = ifindex;
 //   bind(fd, (struct sockaddr*) &addr, sizeof(addr));
-//   
-//   free(name);
+//
 //   return 0;
 // }
 import "C"
 
 import (
-    "syscall"
-    "unsafe"
-    "errors"
-    "fmt"
+	"errors"
+	"fmt"
+	"net"
+	"syscall"
+	"unsafe"
 )
 
 const (
-    domain = 29
-    typ = 3
-    proto = 1
-    ADDR_FIELD = uint32(0x1FFFFFFF)
-    REQUEST_FIELD = uint32(0x40000000)
-    ERROR_FIELD = uint32(0x20000000)
-    EXTENDED_FIELD = uint32(0x80000000)
+	domain         = 29
+	typ            = 3
+	proto          = 1
+	ADDR_FIELD     = uint32(0x1FFFFFFF)
+	REQUEST_FIELD  = uint32(0x40000000)
+	ERROR_FIELD    = uint32(0x20000000)
+	EXTENDED_FIELD = uint32(0x80000000)
 )
 
 type CANFrame struct {
-	ID uint32
-	Len uint32
+	ID   uint32
+	Len  uint32
 	Data [8]byte
 }
 
@@ -84,47 +73,50 @@ func (fr *CANFrame) Error(set bool) {
 	} else {
 		fr.ID &= ^ERROR_FIELD
 	}
-} 
+}
 
 func (fr *CANFrame) IsExtended() bool {
 	return (fr.ID & EXTENDED_FIELD) > 0
 }
 
 func (fr *CANFrame) String() string {
-    return fmt.Sprintf("CANFrame{Addr: %#x, IsRequest: %v, Len: %d, Data: %#x}", fr.Addr(), fr.IsRequest(), fr.Len, fr.Data)
+	return fmt.Sprintf("CANFrame{Addr: %#x, IsRequest: %v, Len: %d, Data: %#x}", fr.Addr(), fr.IsRequest(), fr.Len, fr.Data)
 }
 
 type CANBus struct {
-    name   string
-    sock    int
+	name string
+	sock int
 }
 
-func NewCANBus(name string) (*CANBus, error) {
-    cb := &CANBus{name: name}
-    var err error
-    if cb.sock, err = syscall.Socket(domain, typ, proto); err != nil {
-        return cb, err
-    }
-    if C.setupCAN(C.int(cb.sock), C.CString(cb.name)) != 0 {
-	return cb, errors.New("CGo Errror")
-    }
-    return cb, nil
+func NewCANBus(name string) (cb *CANBus, err error) {
+	cb = &CANBus{name: name}
+	// Create socket
+	if cb.sock, err = syscall.Socket(domain, typ, proto); err != nil {
+		return cb, err
+	}
+	// Find interface with name
+	iface, err := net.InterfaceByName(name)
+	if err != nil {
+		return cb, err
+	}
+	if C.setupCAN(C.int(cb.sock), C.int(iface.Index)) != 0 {
+		return cb, errors.New("CGo Errror")
+	}
+	return cb, nil
 }
 
 func (cb *CANBus) Write(fr *CANFrame) error {
 	buf := (*[16]byte)(unsafe.Pointer(fr))
-	if _, err:= syscall.Write(cb.sock, buf[:]); err != nil {
+	if _, err := syscall.Write(cb.sock, buf[:]); err != nil {
 		return err
 	}
 	return nil
 }
 
 func (cb *CANBus) Read(fr *CANFrame) error {
-    buf := (*[16]byte)(unsafe.Pointer(fr))
-    if _, err := syscall.Read(cb.sock, buf[:]); err != nil {
-        return err
-    }
-    return nil
+	buf := (*[16]byte)(unsafe.Pointer(fr))
+	if _, err := syscall.Read(cb.sock, buf[:]); err != nil {
+		return err
+	}
+	return nil
 }
-
-
